fb5f4f9a97501d8d6bbdfd56b400cc4433565c6a
Joseph Pauly <49600051+jbpauly@users.noreply.github.com>
2021-02-23 14:10:35 -0600

    Bump version: 0.0.1 → 0.0.2

--- fasting/__init__.py
+++ fasting/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Joe Pauly"""
 __email__ = 'joseph.b.pauly@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jbpauly/fasting',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
