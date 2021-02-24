320031843a7bda1b03f6bf68fed209a0c517ca3e
Joseph Pauly <49600051+jbpauly@users.noreply.github.com>
2021-02-23 21:03:26 -0600

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jbpauly/fasting',
-    version='0.0.2',
+    version='0.1.0',
     zip_safe=False,
 )
